b2a8d447171a107b11d061310f3f330547d82e0e
sgnls <sgnls@users.noreply.github.com>
2021-03-04 12:44:05 +0000

    Update exchange-0days-202103.ps1

--- exchange-0days-202103.ps1
+++ exchange-0days-202103.ps1
@@ -4,8 +4,8 @@ write-host -no "$env:computername | Checking IoCs for CVE-2021-26858... "
 findstr /snip /c:”Download failed and temporary file” “%PROGRAMFILES%\Microsoft\Exchange Server\V15\Logging\OABGeneratorLog\*.log” | % { 
     $cve26858.add("$_")
 }
-if(($cve26858).count -gt 0){
-    "Leveraged";
+if($(($cve26858).count) -gt 0){
+    "Leveraged ($(($cve26858).count))";
 }
 else{
     "Not Leveraged";
@@ -16,8 +16,8 @@ write-host -no "$env:computername | Checking IoCs for CVE-2021-26857... "
 get-eventlog -logname Application -source "MSExchange Unified Messaging" -entrytype Error | ? { $_.message -like "*System.InvalidCastException*" } | % {
     $cve26857.add("$_")
 }
-if(($cve26857).count -gt 0){
-    "Leveraged";
+if($(($cve26857).count) -gt 0){
+    "Leveraged ($(($cve26857).count))";
 }
 else{
     "Not Leveraged";
@@ -28,8 +28,8 @@ write-host -no "$env:computername | Checking IoCs for CVE-2021-27065... "
 select-string -path "$env:PROGRAMFILES\Microsoft\Exchange Server\V15\Logging\ECP\Server\*.log" -pattern ‘set-.+VirtualDirectory’ | % { if($_ -match "script"){$cve27065="$_"}} | % {
     $cve27065.add("$_")
 }
-if(($cve27065).count -gt 0){
-    "Leveraged";
+if($(($cve27065).count) -gt 0){
+    "Leveraged ($(($cve27065).count))";
 }
 else{
     "Not Leveraged";
@@ -40,26 +40,26 @@ write-host -no "$env:computername | Checking IoCs for CVE-2021-26855... "
 import-csv -path (gci -recurse -path "$env:PROGRAMFILES\Microsoft\Exchange Server\V15\Logging\HttpProxy" -filter ‘*.log’).fullname | ? { $_.AnchorMailbox -like ‘ServerInfo~*/*’ } | % {
     $cve26855.add("$(($_).datetime)|$(($_).anchormailbox)|$(($_).urlstem)|$(($_).clientipaddress)|$(($_).requestbytes)|$(($_).responsebytes)")
 }
-if(($cve26855).count -gt 0){
-    "Leveraged";
+if($(($cve26855).count) -gt 0){
+    "Leveraged ($(($cve26855).count))";
 }
 else{
     "Not Leveraged";
 } 
 
-if(($cve26858).count -gt 0){
+if($(($cve26858).count) -gt 0){
 	"# CVE-2021-26858 Artefacts"
 	$cve26858
 }
-if(($cve26857).count -gt 0){
+if($(($cve26857).count) -gt 0){
 	"# CVE-2021-26857 Artefacts"
 	$cve26857
 }
-if(($cve27065).count -gt 0){
+if($(($cve27065).count) -gt 0){
 	"# CVE-2021-27065 Artefacts"
 	$cve27065
 }
-if(($cve26855).count -gt 0){
+if($(($cve26855).count) -gt 0){
 	"# CVE-2021-26855 Artefacts"
 	$cve26855
 }
